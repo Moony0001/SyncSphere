e8043a7a5e44ea946a2c43071864bbe500f53564
BhuveshRaina <165366211+BhuveshRaina@users.noreply.github.com>
2024-11-10 14:23:25 +0530

    Add files via upload

---
+++ frontend/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import aim from './aim.png'
+const Profile = () => {
+  return (
+    <div className="user-profile1">
+      <div className="profile-avatar1">
+        <span className="avatar-text">P</span>
+      </div>
+      <h2 className="profile-name">Sid B</h2>
+      <div className='text' >
+      <span className="location-icon">
+            <img src={aim}/>
+      </span>
+      <p className="profile-location">
+        <span>
+        Malhanwada, Madhya Pradesh, India
+        </span>
+      </p>
+      </div>
+      <br />
+      <div className="profile-actions">
+        <button className="follow-button">Follow</button>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
